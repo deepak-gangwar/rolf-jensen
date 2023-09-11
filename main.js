2c57d23fc72964cccdad4ff9dd378119664e7d61
Deepak <deepakgangwar4265@gmail.com>
2023-09-11 18:19:20 +0530

    Add gsap and split-type, start animating

--- main.js
+++ main.js
@@ -1,3 +1,46 @@
 import './style.css'
 
-console.log('hello rolf')
+let typeSplit;
+
+// Split the text up
+function runSplit() {
+    typeSplit = new SplitType(".hero-heading, [text-split], [heading-split], [mob-heading]", {
+        types: "lines words"
+    })
+    createAnimation()
+}
+runSplit()
+
+// Update on window resize
+let windowWidth = window.innerWidth
+window.addEventListener("resize", function () {
+    if (windowWidth !== window.innerWidth) {
+        windowWidth = window.innerWidth
+        typeSplit.revert()
+        runSplit()
+        // ScrollTrigger.refresh(true)
+    }
+})
+
+
+// On Page Load
+function createAnimation() {
+    let intro = gsap.timeline({
+        pause: true
+    });
+
+    const lines = document.querySelectorAll('.line')
+    lines.forEach((line, i) => {
+        let words = line.querySelectorAll('.word')
+        intro.from(
+            words,
+            {
+                y: "101%",
+                stagger: { each: 0.12 },
+                ease: "power3.out",
+                duration: 0.8
+            },
+            1 + i / 5
+        )
+    })
+}
